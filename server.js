e19d39fca9c1c3520032bbe50c5409bfd056b243
Ivan Tavarez <40846384+itava0@users.noreply.github.com>
2022-06-08 22:52:37 -0400

    Looks for a user based on their id

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.get("/", (req, res) => {
 });
 
 app.post("/signin", (req, res) => {
-  console
+  console;
   if (
     req.body.email === database.users[0].email &&
     req.body.password === database.users[0].password
@@ -38,18 +38,34 @@ app.post("/signin", (req, res) => {
   }
 });
 
-app.post('/register', (req, res) => {
+app.post("/register", (req, res) => {
   const { email, name, password } = req.body;
   database.users.push({
-    id: "123",
+    id: "125",
     name: name,
     email: email,
     password: password,
     entries: 0,
     joined: new Date(),
-  })
+  });
   res.json(database.users[database.users.length - 1]);
-})
+});
+
+app.get("/profile/:id", (req, res) => {
+  const { id } = req.params;
+  let found = false;
+  database.users.forEach((user) => {
+    if (user.id === id) {
+      found = true;
+      return res.json(user);
+    }
+  });
+  if (!found) {
+    res.status(404).json("User not found");
+  }
+});
+
+
 
 app.listen(3000, () => {
   console.log("app is running on port 3000");
