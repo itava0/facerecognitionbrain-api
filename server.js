31f350170dfb0fbc33896b38a404e379e70d748c
Ivan Tavarez <40846384+itava0@users.noreply.github.com>
2022-06-10 22:42:45 -0400

    removed the fake database

--- server.js
+++ server.js
@@ -18,36 +18,8 @@ const db = knex({
 app.use(express.json());
 app.use(cors());
 
-const database = {
-  users: [
-    {
-      id: "123",
-      name: "John",
-      email: "john@gmail.com",
-      password: "cookies",
-      entries: 0,
-      joined: new Date(),
-    },
-    {
-      id: "124",
-      name: "Sally",
-      email: "Sally@gmail.com",
-      password: "bananas",
-      entries: 0,
-      joined: new Date(),
-    },
-  ],
-  login: [
-    {
-      id: "987",
-      hash: "",
-      email: "john@email.com",
-    },
-  ],
-};
-
 app.get("/", (req, res) => {
-  res.send(database.users);
+  res.send('success')
 });
 
 app.post("/signin", (req, res) => {
